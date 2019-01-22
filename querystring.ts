a9b06dbf774c52b771b4ff217244cb73628077e5
zongying.cao <zongying.cao@dxc.com>
2019-01-22 09:59:44 +0000

    improve the query string
    Signed-off-by: zongying.cao <zongying.cao@dxc.com>

--- querystring.ts
+++ querystring.ts
@@ -1,5 +1,14 @@
 import * as _ from 'underscore';
 
 export function encodeQuerystring(obj: Object): any {
-    return obj == null || _.isEmpty(obj) ? null :  encodeURI(_.map(_.pairs(obj),pair=>`${pair[0]}=${pair[1]}`).join('&'));
+    let removeEmpty = function(list: any):any{
+        return list == null ? null:  _.filter(list, item=>!_.isEmpty(item[1]));
+    };
+
+    let encode = function(list: any): string {
+        return list == null || _.isEmpty(list) ? null : encodeURI(_.map(list, pair=>`${pair[0]}=${pair[1]}`).join('&'));
+    };
+
+    return encode(removeEmpty(obj == null || _.isEmpty(obj) ? null :_.pairs(obj)));
+    
 }
